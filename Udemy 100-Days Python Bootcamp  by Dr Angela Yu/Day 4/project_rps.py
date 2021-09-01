8d2c858050ce86031e306ffab5abe0759f866dfb
Senthil Kumar Kanagaraj <senthilkumark.nitt@gmail.com>
2021-09-01 23:05:07 +0530

    Update project_rps.py

--- Udemy 100-Days Python Bootcamp  by Dr Angela Yu/Day 4/project_rps.py
+++ Udemy 100-Days Python Bootcamp  by Dr Angela Yu/Day 4/project_rps.py
@@ -11,7 +11,7 @@ draw = bot = user = 0
 while(play_continue):
     user_choice = int(input("What to Select: Rock - 0 or Paper - 1 or Scissor - 2 ?\n"))
     
-    if user_choice > 2:
+    if user_choice > 2 or user_choice < 0:
         print("You have entered wrong choice! You Lost!!!")
         break
     
